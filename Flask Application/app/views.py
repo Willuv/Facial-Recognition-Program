ca4ac09f8ab38e3f33ea2163ed3cf4c5c2497232
William Uvlin <Will.Uvlin@gmail.com>
2024-08-06 23:13:57 -0400

    Link ML model to website to predict gender in images

--- Flask Application/app/views.py
+++ Flask Application/app/views.py
@@ -1,7 +1,10 @@
+import os
+import cv2
+from app.face_recognition import faceRecognitionPipeline
 from flask import render_template, request
 
 
-
+UPLOAD_FOLDER = 'static/upload'
 
 def index():
     return render_template('index.html')
@@ -10,4 +13,17 @@ def app():
     return render_template('app.html')
 
 def genderapp():
-    return render_template('gender.html')
+    if request.method == 'POST':
+        f = request.files['image_name']
+        filename = f.filename
+        # save our image in upload folder
+        path = os.path.join(UPLOAD_FOLDER,filename)
+        f.save(path) #save image into upload folder
+        # get predictions 
+        pred_image, predictions = faceRecognitionPipeline(path)
+        pred_filename = 'prediction_image.jpg'
+        cv2.imwrite(f'./static/predict/{pred_filename}',pred_image)
+        
+        print('ML model predicted sucessfully')
+
+    return render_template('gender.html') # GET REQUEST
