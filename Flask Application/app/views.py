fe848ac94edd482159b3a900365210be31368f11
William Uvlin <Will.Uvlin@gmail.com>
2024-08-06 22:51:32 -0400

    create app.html and link all html pages together

--- Flask Application/app/views.py
+++ Flask Application/app/views.py
@@ -5,3 +5,6 @@ from flask import render_template, request
 
 def index():
     return render_template('index.html')
+
+def app():
+    return render_template('app.html')
--- Flask Application/main.py
+++ Flask Application/main.py
@@ -4,6 +4,7 @@ from app import views
 app = Flask(__name__) # webserver gateway interphase (WSGI)
 
 app.add_url_rule(rule='/',endpoint='home',view_func=views.index)
+app.add_url_rule(rule='/app/',endpoint='app',view_func=views.app)
 
 if __name__ == "__main__":
     app.run(debug=True)
